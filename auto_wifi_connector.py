4adba4cf0a7f42999d7a65a8f77e33dadef42cea
Sourakanti Mandal <52707495+Sonu64@users.noreply.github.com>
2020-08-11 00:00:33 +0530

    uploaded auto_wifi_connector.py

---
+++ auto_wifi_connector.py
@@ -0,0 +1,40 @@
+# Importing required modules
+import os, sys
+
+# Getting all the Saved networks and showing them. read() function helps in storing that
+savedProfiles = os.popen("netsh wlan show profiles").read()
+print("ALL SAVED NETWORKS\n"+savedProfiles)
+
+# Getting all the Available networks and showing them
+availableProfiles = os.popen("netsh wlan show networks").read()
+print("\nALL AVAILABLE NETWORKS\n"+availableProfiles)
+
+# Getting preferred connection from the user
+preferredSSID = input("Enter the preferred WiFi for your connection: ")
+# Disconnecting from all other connections
+response = os.popen("netsh wlan disconnect").read()
+
+# If the preferred SSID is not saved in System, then notify and exit
+if preferredSSID not in savedProfiles:
+    print("Profile for " + preferredSSID + " is not saved in your system!")
+    print("Sorry, But I can't connect to this network :(")
+    input("Press the ENTER key to exit...")
+    sys.exit()
+
+# Otherwise continue to check untill an available network is not found
+while True:
+    avail = os.popen("netsh wlan show networks").read()
+    # If the preffered SSID is available to connect to
+    if preferredSSID in avail:
+        # just showing that the network is found, i.e., It is Turned ON or whatever !
+        print("Network Found!")
+        # No need to check for available networks any more
+        break;
+
+# Printing a decoraive text
+print("Connecting....")
+# Running the command to connect to that network via netsh
+connectionResponse = os.popen('netsh wlan connect name = '+'"'+preferredSSID+'"').read()
+# If you wish, print it
+print(connectionResponse)
+        
